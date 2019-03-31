1036cabc20ea19e63624017b08b7f2965008b6ba
Alex Chao <chao.alexander.123@gmail.com>
2019-03-31 17:52:38 +0000

    create reverseString function and pass the tests

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Code your solution here!
 function printString(string) {
   console.log(string[0])
-  if (string.length > 1) {
-    var newString = string.slice(1)
-    printString(newString)
-  } else {
-    return true
-  }
+  if (string.length === 1) return true
+  printString(string.slice(1))
 }
 
 function reverseString(string) {
+  if (string.length === 1) return string
+  return string.slice(-1) + reverseString(string.slice(0, -1))
+}
+
+function isPalindrome(string) {
   
 }
