d9413ede72b7501c86cb9bea8db8ba5612a36674
Alex Chao <chao.alexander.123@gmail.com>
2019-03-31 17:17:54 +0000

    create printString function to recursively print each letter

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Code your solution here!
+function printString(string) {
+  console.log(string[0])
+  if (string.length > 1) {
+    var newString = string.slice(1)
+    printString(newString)
+  } else {
+    return true
+  }
+}
 
+function reverseString(string) {
+  
+}
